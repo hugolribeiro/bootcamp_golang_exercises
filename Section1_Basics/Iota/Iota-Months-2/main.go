716f8c14e99e47bc7173eb1d4647a00fbfe34577
Hugo <hugolerib@gmail.com>
2022-07-03 20:59:30 -0300

    add basic strings and iota exercises

---
+++ Section1_Basics/Iota/Iota-Months-2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Iota Months #2
+//
+//  1. Initialize multiple constants using iota.
+//  2. Please follow the instructions inside the code.
+//
+// EXPECTED OUTPUT
+//  1 2 3
+// ---------------------------------------------------------
+
+func main() {
+	// Now, use iota and initialize the following constants
+	// "automatically" to 1, 2, and 3 respectively.
+	const (
+		_ = iota
+		Jan
+		Feb
+		Mar
+	)
+
+	// This should print: 1 2 3
+	// Not: 0 1 2
+	fmt.Println(Jan, Feb, Mar)
+}
